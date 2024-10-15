d8a2f685f242950a60b953e16c1aa170f24d4fae
Adam Smith <sogaxili@gmail.com>
2024-10-15 16:37:03 +0800

    style: 优化艺术空间界面

--- JetpackComposeExample/app/src/main/java/com/example/jetpackcomposeexample/screens/ArtSpaceScreen.kt
+++ JetpackComposeExample/app/src/main/java/com/example/jetpackcomposeexample/screens/ArtSpaceScreen.kt
@@ -8,10 +8,16 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
+import androidx.compose.foundation.layout.calculateEndPadding
+import androidx.compose.foundation.layout.calculateStartPadding
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawing
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
@@ -26,6 +32,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
@@ -37,7 +44,19 @@ import com.example.jetpackcomposeexample.data.ArtSpaceData
 
 @Composable
 fun ArtSpaceScreen() {
-    CommonScreen(title = "艺术空间") {
+    val layoutDirection = LocalLayoutDirection.current
+
+    Surface(
+        modifier = Modifier
+            .fillMaxSize()
+            .statusBarsPadding()
+            .padding(
+                start = WindowInsets.safeDrawing.asPaddingValues()
+                    .calculateStartPadding(layoutDirection),
+                end = WindowInsets.safeDrawing.asPaddingValues()
+                    .calculateEndPadding(layoutDirection)
+            )
+    ) {
         ArtSpace()
     }
 }
